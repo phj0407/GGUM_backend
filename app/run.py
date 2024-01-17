b271dd097804d156aa9d1da3c3628f84d46b0df2
phj0407 <hyejin2371@gmail.com>
2024-01-17 13:01:00 +0900

    feat : import 문 수정, app.py 정리

--- app/run.py
+++ app/run.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# 현재 파일의 경로를 가져와서 프로젝트 폴더를 sys.path에 추가
+project_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, project_folder)
+
 from app import app
 
 if __name__ == '__main__':
