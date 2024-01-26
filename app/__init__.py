00b2c2183821091a51675de87a296a0c407d41ab
phj0407 <hyejin2371@gmail.com>
2024-01-26 16:41:12 +0900

    feat : CORS 이슈 해결

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,11 @@ from provider.user import bp_user
 from provider.diary import bp_diary
 from provider.notice import bp_notice
 from provider.survey import bp_survey
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 bind_api = Blueprint('api', __name__, url_prefix='/api')
 
 app.register_blueprint(bp_user)
